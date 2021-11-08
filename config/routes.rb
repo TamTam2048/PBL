d79aacb81d5b3104552fac3865e11c7f8d4c4237
TamTam2048 <luong.minh.tam@moneyforward.co.jp>
2021-11-08 17:47:57 +0700

    Added method and rspec test for products_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  resources :users
   resources :products
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#blank_star" do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:product) { FactoryBot.create(:product, user: user, rating: 4.0) }
+
+    it "returns correct number of blank stars " do
+      expect(product.blank_stars).to eq(1)
+    end
+  end
 end
