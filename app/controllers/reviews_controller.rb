3a34e3cd205aef2c9931ee6ffa2c2f022e507023
TamTam2048 <luong.minh.tam@moneyforward.co.jp>
2021-11-17 11:05:11 +0700

    Added edit and detroy method for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
-  before_action :set_product, only: %i[new create]
+  before_action :set_product
+  before_action :set_review, only: %i[edit update destroy]
 
   def new
     @review = Review.new
@@ -12,12 +13,35 @@ class ReviewsController < ApplicationController
     @review.product_id = @product.id
     if @review.save
       @product.update_product_rating
+      @review = Review.new
     else
       redirect_to product_path(@product)
     end
   end
 
+  def edit; end
+
+  def update
+    @review.update(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+      flash[:success] = "Your comment has been updated"
+    else
+      redirect_to edit_product_review_path(@product.id, @review.id)
+      flash[:success] = "An error occured. Please try again"
+    end
+  end
+
+  def destroy
+    @review.destroy
+    flash[:success] = "Your comment has been deleted"
+  end
+
   private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 
   def set_product
     @product = Product.find_by(id: params[:product_id])
