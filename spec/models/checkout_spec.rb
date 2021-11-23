2463d4b0a33ef141d5230b78d3e9bc50b106387d
TamTam2048 <luong.minh.tam@moneyforward.co.jp>
2021-11-23 18:25:01 +0700

    Working

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -3,5 +3,23 @@
 require "rails_helper"
 
 RSpec.describe Checkout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  let(:card_token) {
+    Stripe::Token.create({ card: { number: "4242424242424242", exp_month: 11, exp_year: 2025, cvc: "314" } })
+  }
+
+  describe "#create_charge" do
+    it "creates a customer" do
+      customer = Stripe::Customer.create({
+                                           email: user.email,
+                                           source: card_token
+                                         })
+      expect(customer.email).to eq(user.email)
+      # expect do
+      #   Stripe::Charge.create(customer: customer.id, amount: 200, currency: "usd")
+      # end.not_to raise_error
+    end
+
+  end
 end
