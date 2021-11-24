42a1f622bdd5eb8f610fc929c8deea6690efafb3
TamTam2048 <luong.minh.tam@moneyforward.co.jp>
2021-11-24 16:36:43 +0700

    Adding admin user page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :products
     resources :checkouts
     resources :orders
+    resources :users
   end
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
