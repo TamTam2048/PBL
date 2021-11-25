1cffc4b98ebb0705b59ddc12070f5051fc093bd0
TamTam2048 <luong.minh.tam@moneyforward.co.jp>
2021-11-25 16:51:26 +0700

    Finished admin dashboard page and user profile page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,6 @@ class ProductsController < ApplicationController
   before_action :set_product, only: %i[show edit update destroy]
   before_action :valid_user,  only: %i[edit update destroy]
 
-
   def index
     @products = Product.all
     @line_item = current_order.line_items.new
