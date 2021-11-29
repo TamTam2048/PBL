3dc5cbdcffc8208b2497ab1f3b48f98bd463d906
TamTam2048 <luong.minh.tam@moneyforward.co.jp>
2021-11-29 08:44:32 +0700

    Added checkouts_controller_spec

--- spec/controllers/checkouts_controller_spec.rb
+++ spec/controllers/checkouts_controller_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe CheckoutsController, type: :controller do
--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -5,9 +5,9 @@ require "rails_helper"
 RSpec.describe Checkout, type: :model do
   let(:user) { FactoryBot.create(:user) }
 
-  let(:card_token) {
+  let(:card_token) do
     Stripe::Token.create({ card: { number: "4242424242424242", exp_month: 11, exp_year: 2025, cvc: "314" } })
-  }
+  end
 
   describe "#create_charge" do
     it "creates a customer" do
@@ -17,7 +17,8 @@ RSpec.describe Checkout, type: :model do
 
     it "creates a charge" do
       customer = Stripe::Customer.create({ email: user.email, source: card_token })
-      charge = Stripe::Charge.create(customer: customer.id, amount: 500, description: "user #{customer.email} has started a purchase", currency: "usd")
+      charge = Stripe::Charge.create(customer: customer.id, amount: 500,
+                                     description: "user #{customer.email} has started a purchase", currency: "usd")
       allow(charge).to receive(:status).and_return(charge.status)
       expect(charge.status).to eq("succeeded")
     end
